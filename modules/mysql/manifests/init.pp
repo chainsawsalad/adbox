eca604f6a3aca7c49d4df11cfa67e184d7bfb7b0
David Joy <davjoy@gmail.com>
2014-06-06 14:57:20 -0400

    Adding localstats to etc/hosts file, and adding DB perms for it.

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -41,4 +41,11 @@ class mysql
           command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'grant all on `database`.* to `root@localhost`;'",
           require => [Service["mysql"], Exec["create-default-db"]]
   }
+
+  exec
+  {
+    "grant-default-db-host-name":
+    command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'grant all on *.* to `root`@`localstats.nanigans.com`;'",
+    require => [Service["mysql"], Exec["set-mysql-password"]]
+  }
 }
