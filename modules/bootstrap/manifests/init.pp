b5a3b46ea42fd3f140bc7004e0d6f77ee0220c59
Todd Dukart <tdukart@nanigans.com>
2014-08-07 15:44:58 -0400

    Run an apt-get upgrade during provisioning to update a few base packages

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -9,4 +9,9 @@ class bootstrap {
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update'
   }
+  
+  exec { 'apt-get upgrade': 
+    command => '/usr/bin/apt-get upgrade -y',
+    require => Exec["apt-get update"]
+  }
 }
